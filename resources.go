bfd84de4c83639e8b18c30127264590ea46652a7
jorge.marey <jorge.marey@bq.com>
2015-08-12 23:21:10 +0200

    Added api method to update ACL. Included proper tests.

--- resources.go
+++ resources.go
@@ -3,7 +3,7 @@ package corbel
 // ResourcesService handles the interface for retrival resource's representation
 // on Corbel.
 //
-// Full API info: http://docs.silkroadresources.apiary.io/
+// Full API info: http://docs.corbelresources.apiary.io/
 type ResourcesService struct {
 	client *Client
 }
