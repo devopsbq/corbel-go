fd911551bc3362667a02ca6796f20aba3c6aadd4
jorge.marey <jorge.marey@bq.com>
2016-01-13 11:25:41 +0100

    Annonimize structs on searches

--- constants.go
+++ constants.go
@@ -2,4 +2,8 @@ package corbel
 
 const (
 	grantType = "urn:ietf:params:oauth:grant-type:jwt-bearer"
+	// SortAsc used for sorting (ascending)
+	SortAsc = "asc"
+	// SortDesc used for sorting (descending)
+	SortDesc = "desc"
 )
